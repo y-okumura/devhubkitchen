f5e7abf0fd0bca46cbdac4ddb163bd42edd18bd2
Y Okumura <y-okumura@nichiwa-system.co.jp>
2017-03-16 00:44:09 +0900

    Devhub起動するようになった

--- test/integration/default/serverspec/node_spec.rb
+++ test/integration/default/serverspec/node_spec.rb
@@ -2,6 +2,10 @@ require 'serverspec'
 
 set :backend, :exec
 
-describe port(3000) do
-  it { should be_listening }
+describe service(:devhub) do
+  it { should be_running }
+end
+
+describe command('curl http://127.0.0.1:3000 -o /dev/null -w "%{http_code}\n" -s') do
+  its(:stdout) { should match /^200$/ }
 end
